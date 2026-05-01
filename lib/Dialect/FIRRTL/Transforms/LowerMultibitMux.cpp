40858a33ef75b85ebf2b00977ec5531754dec678
wmatt <matt@weingarten.org>
2026-05-01 15:34:37 -0400

    Fix multibix lowering that fixes Bootrom compilation error that breaks simulation

--- lib/Dialect/FIRRTL/Transforms/LowerMultibitMux.cpp
+++ lib/Dialect/FIRRTL/Transforms/LowerMultibitMux.cpp
@@ -26,18 +26,14 @@ void LowerMultibitMuxPass::runOnOperation() {
   bool anyFailure = false;
 
   llvm::SmallVector<circt::firrtl::MultibitMuxOp> multibitMuxes;
-  module.walk(
-      [&](circt::firrtl::MultibitMuxOp op) { multibitMuxes.push_back(op); });
-
-  // Cheap unique id in case we decide to materialize any debug names later.
-  unsigned uniqueId = 0;
-  auto makeGeneratedName = [&](llvm::StringRef base) -> std::string {
-    return (base + "_MB_LOWER_" + std::to_string(uniqueId++)).str();
-  };
+  module.walk([&](circt::firrtl::MultibitMuxOp op) {
+    multibitMuxes.push_back(op);
+  });
 
   for (auto op : multibitMuxes) {
     mlir::OpBuilder builder(op);
     auto loc = op.getLoc();
+
     mlir::Value index = op.getIndex();
     auto inputs = op.getInputs();
 
@@ -54,16 +50,6 @@ void LowerMultibitMuxPass::runOnOperation() {
       continue;
     }
 
-    // FIRRTL/CIRCT multibit_mux ordering is:
-    //   index == 0 selects inputs.back()
-    //   index == 1 selects inputs[inputs.size()-2]
-    //   ...
-    // Reverse first so that level[0] corresponds to logical input 0.
-    llvm::SmallVector<mlir::Value> level;
-    level.reserve(inputs.size());
-    for (auto it = inputs.begin(); it != inputs.end(); ++it)
-      level.push_back(*it);
-
     auto indexType = llvm::dyn_cast<circt::firrtl::UIntType>(index.getType());
     if (!indexType) {
       op.emitError() << "expected multibit_mux index to be !firrtl.uint, got "
@@ -79,65 +65,96 @@ void LowerMultibitMuxPass::runOnOperation() {
       continue;
     }
 
-    // Match FIRRTLLowering behavior:
-    // resize the selector to the width required by the number of inputs.
-    auto requiredIndexWidth =
-        llvm::Log2_64_Ceil(static_cast<uint64_t>(inputs.size()));
-    auto resizedIndexTy =
-        circt::firrtl::UIntType::get(builder.getContext(), requiredIndexWidth);
+    // The maximum selector value we need to materialize is inputs.size() - 1.
+    // Make sure it is representable by the index width.
+    //
+    // Example:
+    //   width 1 can select values 0..1, so it can only address 2 inputs.
+    //   width 3 can select values 0..7, so it can address 8 inputs.
+    if (indexWidth < 64 && inputs.size() > (uint64_t(1) << indexWidth)) {
+      op.emitError() << "firrtl.multibit_mux has " << inputs.size()
+                     << " inputs, but index width " << indexWidth
+                     << " can only select " << (uint64_t(1) << indexWidth)
+                     << " inputs";
+      anyFailure = true;
+      continue;
+    }
 
-    mlir::Value resizedIndex = index;
-    if (indexWidth < requiredIndexWidth) {
-      resizedIndex = builder
-                         .create<circt::firrtl::PadPrimOp>(
-                             loc, resizedIndexTy, index, requiredIndexWidth)
-                         .getResult();
-    } else if (indexWidth > requiredIndexWidth) {
-      resizedIndex =
+    // Mux definition is:
+    //
+    //   firrtl.multibit_mux %index, %v_(n-1), ..., %v_1, %v_0
+    //
+    // where:
+    //
+    //   index == 0 selects %v_0
+    //   index == 1 selects %v_1
+    //   ...
+    //
+    // The IR operand list is physically reversed relative to selector value.
+    // Reverse it here so:
+    //
+    //   logicalInputs[0] == value selected by index == 0
+    //   logicalInputs[1] == value selected by index == 1
+    //   ...
+    llvm::SmallVector<mlir::Value> logicalInputs;
+    logicalInputs.reserve(inputs.size());
+    
+    for (size_t i = inputs.size(); i > 0; --i)
+      logicalInputs.push_back(inputs[i - 1]);
+    
+    mlir::Value result = logicalInputs[0];
+
+    auto eqType = circt::firrtl::UIntType::get(builder.getContext(), 1);
+
+    for (uint64_t selectorValue = 1, e = logicalInputs.size();
+         selectorValue < e; ++selectorValue) {
+
+
+      // Build constant for each possible index
+      auto constAttrType = mlir::IntegerType::get(
+          builder.getContext(),
+          indexWidth,
+          mlir::IntegerType::Unsigned);
+      
+      auto constAttr = mlir::IntegerAttr::get(
+          constAttrType,
+          llvm::APInt(indexWidth, selectorValue));
+
+      mlir::Value constIndex =
           builder
-              .create<circt::firrtl::BitsPrimOp>(loc, resizedIndexTy, index,
-                                                 /*hi=*/requiredIndexWidth - 1,
-                                                 /*lo=*/0)
+              .create<circt::firrtl::ConstantOp>(
+                  loc,
+                  index.getType(),
+                  constAttr)
+              .getResult();
+      // Equals check for each index
+      mlir::Value eq =
+          builder
+              .create<circt::firrtl::EQPrimOp>(
+                  loc,
+                  eqType,
+                  index,
+                  constIndex)
+              .getResult();
+      // Build the mux chain
+      result =
+          builder
+              .create<circt::firrtl::MuxPrimOp>(
+                  loc,
+                  result.getType(),
+                  eq,
+                  logicalInputs[selectorValue],
+                  result)
               .getResult();
     }
 
-    // Extract one selector bit at a time and reduce pairs with firrtl.mux.
-    //
-    // For each stage:
-    //   next[i] = mux(bit_k, level[2*i + 1], level[2*i])
-    //
-    // If there is an odd leftover element, carry it to the next level.
-    for (int64_t bit = 0; level.size() > 1; ++bit) {
-      auto bitTy = circt::firrtl::UIntType::get(builder.getContext(), 1);
-      mlir::Value selBit = builder
-                               .create<circt::firrtl::BitsPrimOp>(
-                                   loc, bitTy, resizedIndex, /*hi=*/bit,
-                                   /*lo=*/bit)
-                               .getResult();
-
-      llvm::SmallVector<mlir::Value> nextLevel;
-      nextLevel.reserve((level.size() + 1) / 2);
-
-      for (size_t i = 0, e = level.size(); i < e; i += 2) {
-        if (i + 1 < e) {
-          mlir::Value muxVal =
-              builder
-                  .create<circt::firrtl::MuxPrimOp>(
-                      loc, level[i].getType(), selBit, level[i + 1], level[i])
-                  .getResult();
-          nextLevel.push_back(muxVal);
-        } else {
-          nextLevel.push_back(level[i]);
-        }
-      }
-
-      level = std::move(nextLevel);
-    }
-
-    op.replaceAllUsesWith(level.front());
+    op.replaceAllUsesWith(result);
     op.erase();
     ++numLoweredMuxes;
   }
+
+  if (anyFailure)
+    signalPassFailure();
 }
 
 namespace circt {
